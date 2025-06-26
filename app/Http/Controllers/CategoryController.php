a210b476bad476b53b29c11ba1bd9b50459e9b87
arifah <arifahrahmaniyah28@gmail.com>
2025-06-26 21:43:17 +0700

    saya suidah selesai membuat_create_kategori

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -19,7 +19,7 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        //
+         return view('petugas.kategori.create');
     }
 
     /**
@@ -27,8 +27,20 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+         $request->validate([
+            'nama_kategori' => 'required|max:255',
+
+        ], [
+            'nama_kategori.required' => 'kategori wajib diisi.',
+            
+        ]);
+        Category::create($request->all());
+        // manggail field biar gaharus satu satu 
+        
+
+        return redirect()->route('kategori.index')->with('success', 'Kategori berhasil ditambahkan');
     }
+
 
     /**
      * Display the specified resource.
---
+++ resources/views/petugas/kategori/create.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.mantis')
+
+@section('content')
+<div class="container">
+    <div class="card mt-3">
+        <div class="card-header d-flex justify-content-between align-items-center">
+            <h3 class="card-title mb-0">Tambah Kategori</h3>
+            <a href="{{ route('kategori.index') }}" class="btn btn-secondary">Kembali</a>
+        </div>
+        <div class="card-body">
+            <form action="{{ route('kategori.store') }}" method="POST">
+                @csrf
+                
+                <!-- Nama Mahasiswa -->
+                <div class="form-group my-2">
+                    <label for="nama_kategori">Nama Kategori <span class="text-danger">*</span></label>
+                    <input type="text" 
+                           class="form-control @error('nama_kategori') is-invalid @enderror" 
+                           name="nama_kategori" 
+                           id="nama_kategori" 
+                           value="{{ old('nama_kategori') }}" 
+                           placeholder="Masukkan nama KATEGORI"
+                           autofocus>
+                    @error('nama_kategori')
+                        <small class="text-danger">{{ $message }}</small>
+                    @enderror
+                </div>
+                   
+
+
+
+                <div class="d-flex justify-content-between mt-4">
+                    <a href="{{ route('kategori .index') }}" class="btn btn-secondary">Batal</a>
+                    <button type="submit" class="btn btn-primary">
+                        <i class="fas fa-save"></i> Simpan 
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+
+@endsection
